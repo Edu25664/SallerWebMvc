242df64b1adc4d147c65fe518e3f1179873f9b02
Eduardo Oliveira Ramos <eduardo.oliveiraramos12@hotmail.com>
2022-08-28 17:51:34 -0300

    first commit

---
+++ SallerWebMvc/Models/Departments.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SallerWebMvc.Controllers.entities
+{
+    public class Departments
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
